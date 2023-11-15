3c64b1d0feee437c38e42bdecad8a82a748feeff
ErmakovEv <ej-88@ya.ru>
2023-11-15 22:29:11 +0300

    fix zaz

--- src/components/Car/Car.tsx
+++ src/components/Car/Car.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { a } from '@react-spring/three';
-import Model from '../Model/Model.jsx';
+import Model from '../Model/ModelDraco1';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import CATEGORIESMAP from '../utils/utils';
