ed3a0ec9883163195085dabd581b6697be861d74
ErmakovEv <ej-88@ya.ru>
2023-11-19 23:23:56 +0300

    fix

--- src/components/SwipeableDrawer/SwipeableDrawer.tsx
+++ src/components/SwipeableDrawer/SwipeableDrawer.tsx
@@ -3,7 +3,7 @@ import { Global } from '@emotion/react';
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { grey } from '@mui/material/colors';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 import Typography from '@mui/material/Typography';
