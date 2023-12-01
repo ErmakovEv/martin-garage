a07d362963b1a017cdd4b9eeb8bbce3d87eee153
ErmakovEv <ej-88@ya.ru>
2023-12-01 13:18:00 +0300

    styles for footer

--- src/components/CustomMap/CustomMap.tsx
+++ src/components/CustomMap/CustomMap.tsx
@@ -1,14 +1,25 @@
-import { YMaps, Map } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+
+/*
+Кастомные значки для PlaceMark
+https://yandex.com/dev/jsapi-v2-1/doc/en/v2-1/ref/reference/option.presetStorage
+*/
 
 function CustomMap() {
   return (
     <div className="map-container">
       <YMaps>
         <Map
-          defaultState={{ center: [55.75, 37.57], zoom: 11 }}
+          defaultState={{ center: [60.080433, 31.046361], zoom: 11 }}
           width="100%"
-          height="32vh"
-        />
+          height="32vh">
+          <Placemark
+            geometry={[60.080433, 31.066361]}
+            options={{
+              preset: `islands#darkGreenRepairShopCircleIcon`,
+            }}
+          />
+        </Map>
       </YMaps>
     </div>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,115 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import CustomMap from "../CustomMap/CustomMap";
+import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
+function Footer() {
+  return (
+    <Box
+      className="footer"
+      component="div"
+      sx={{
+        backgroundColor: "blue",
+        height: "30%",
+        position: "relative",
+      }}>
+      <Box
+        component="div"
+        sx={{
+          position: "absolute",
+          zIndex: "10",
+          width: "65%",
+          height: "90%",
+          display: "flex",
+        }}>
+        <Box component="div" sx={{ width: "10%", opacity: 1 }}></Box>
+        <Box
+          component="div"
+          sx={{
+            background: "#1b1b1b",
+            opacity: 0.8,
+            padding: 2,
+          }}>
+          <Typography sx={{ fontSize: "1.2vh", color: "white" }}>
+            <Box
+              component="div"
+              sx={{ fontFamily: ["Oswald"], textAlign: "center", pb: 2 }}>
+              {"КОНТАКТЫ"}
+            </Box>
+            <Box component="div">
+              <PhoneInTalkIcon
+                sx={{ color: "#29b249", fontSize: "1.2vh", mr: 0.5 }}
+              />
+              {"8 (999) 000-11-22"}
+            </Box>
+
+            <Box component="div">
+              <MarkEmailReadIcon
+                sx={{ color: "#29b249", fontSize: "1.2vh", mr: 0.5 }}
+              />
+              {"martin-garage@gmail.com"}
+            </Box>
+            <Box
+              component="div"
+              sx={{
+                fontFamily: ["Oswald"],
+                textAlign: "center",
+                pt: "2vh",
+                pb: "1vh",
+              }}>
+              {"ВРЕМЯ РАБОТЫ"}
+            </Box>
+
+            <Box component="div" sx={{ fontFamily: ["Oswald"] }}>
+              <AccessTimeIcon
+                sx={{ color: "#29b249", fontSize: "1.2vh", mr: 0.5 }}
+              />
+              {"9:00-21:00 Пн-Сб"}
+            </Box>
+            <Box
+              component="div"
+              sx={{
+                fontFamily: ["Oswald"],
+                textAlign: "center",
+                pt: "2vh",
+                pb: "1vh",
+              }}>
+              {"АДРЕС"}
+            </Box>
+
+            <Box component="div" sx={{ fontFamily: ["Oswald"] }}>
+              <LocationOnIcon
+                sx={{ color: "#29b249", fontSize: "1.2vh", mr: 0.5 }}
+              />
+              {"дер. Коккорево"}
+            </Box>
+          </Typography>
+        </Box>
+      </Box>
+      <CustomMap />
+      <Box
+        component="div"
+        sx={{
+          backgroundColor: "red",
+          position: "absolute",
+          bottom: "0",
+          width: "100%",
+          background: "#1b1b1b",
+          color: "white",
+          fontFamily: ["Oswald"],
+          textAlign: "center",
+          fontSize: "1.2vh",
+          zIndex: "100",
+        }}>
+        © 2024 Автосервис Martin Garage <br />
+        Вся информация, представленная на сайте, носит информационный характер и
+        не является публичной офертой
+      </Box>
+    </Box>
+  );
+}
+
+export default Footer;
