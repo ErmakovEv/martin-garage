605d1b1603cec9486e1d459f035685f0f204f80c
ErmakovEv <ej-88@ya.ru>
2023-12-05 23:49:06 +0300

    test sing in

--- src/components/Pages/SecondPage.tsx
+++ src/components/Pages/SecondPage.tsx
@@ -1,13 +1,13 @@
-import { forwardRef, useState } from 'react';
-import { Box, Button, Modal } from '@mui/material';
-import CustomForm from '../Form/Form';
+import { forwardRef } from 'react';
+import { Box, TextField } from '@mui/material';
+// import CustomForm from '../Form/Form';
 import Footer from '../Footer/Footer';
 import bg from '../../img/aston-martin.jpeg';
 
 const SecondPage = forwardRef<HTMLElement>((_, ref) => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  // const [open, setOpen] = useState(false);
+  // const handleOpen = () => setOpen(true);
+  // const handleClose = () => setOpen(false);
 
   return (
     <section ref={ref} style={{ height: '100%' }}>
@@ -41,7 +41,7 @@ const SecondPage = forwardRef<HTMLElement>((_, ref) => {
               pt: 10,
             }}
           >
-            <Button onClick={handleOpen}>Открыть форму</Button>
+            {/* <Button onClick={handleOpen}>Открыть форму</Button>
             <Modal
               open={open}
               onClose={handleClose}
@@ -49,7 +49,62 @@ const SecondPage = forwardRef<HTMLElement>((_, ref) => {
               aria-describedby="modal-modal-description"
             >
               <CustomForm />
-            </Modal>
+            </Modal> */}
+            <Box
+              component="div"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              minHeight="100vh"
+            >
+              <Box
+                component="div"
+                bgcolor="#fff"
+                maxWidth="420px"
+                boxShadow="6"
+                borderRadius="6px"
+                margin="5px"
+                sx={{ paddingX: { xs: '20px', sm: '30px', xl: '40px' } }}
+                paddingY="40px"
+              >
+                <TextField
+                  id="outlined-basic"
+                  label="e-mail"
+                  // variant="standard"
+                  color="primary"
+                  type="email"
+                  name="user_email"
+                  size="small"
+                  sx={{ width: '100%' }}
+                  InputProps={{ style: { color: 'white' } }}
+                  InputLabelProps={{ style: { fontSize: '1.4vh' } }}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="e-mail"
+                  // variant="standard"
+                  color="primary"
+                  type="email"
+                  name="user_email"
+                  size="small"
+                  sx={{ width: '100%' }}
+                  InputProps={{ style: { color: 'white' } }}
+                  InputLabelProps={{ style: { fontSize: '1.4vh' } }}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="e-mail"
+                  // variant="standard"
+                  color="primary"
+                  type="email"
+                  name="user_email"
+                  size="small"
+                  sx={{ width: '100%' }}
+                  InputProps={{ style: { color: 'white' } }}
+                  InputLabelProps={{ style: { fontSize: '1.4vh' } }}
+                />
+              </Box>
+            </Box>
           </Box>
         </Box>
         <Footer />
