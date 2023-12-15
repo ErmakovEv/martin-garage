514ebd202ae972111e49e6505658ab3bd9087063
ErmakovEv <ej-88@ya.ru>
2023-12-15 09:40:34 +0300

    add footerrr2

--- src/components/Engine/Engine.tsx
+++ src/components/Engine/Engine.tsx
@@ -9,8 +9,8 @@ export default function Engine() {
   const myMesh = useRef<any>();
   const vek = new THREE.Vector3();
   useFrame((state) => {
-    state.camera.position.lerp(vek.set(10, 5, 15), 0.08);
-    // myMesh.current.rotation.y += 0.03;
+    state.camera.position.lerp(vek.set(25, 3, 10), 0.08);
+    myMesh.current.rotation.y += 0.03;
     // myMesh.current.rotation.x += 0.03;
     // myMesh.current.rotation.z += 0.03;
   });
@@ -19,8 +19,8 @@ export default function Engine() {
     <mesh
       ref={myMesh}
       // rotation={[0, -0.5, 0.5]}
-      // position={[-8, -2, 0]}
-      scale={2}
+      position={[0, 2, -2]}
+      scale={0.8}
     >
       <ModelEngine />
     </mesh>
--- src/components/Pages/AboutPage.tsx
+++ src/components/Pages/AboutPage.tsx
@@ -2,53 +2,52 @@ import { Box } from '@mui/material';
 // import CustomForm from '../CustomAutocomplite/CustomAutocomplite';
 import Footer from '../Footer/Footer';
 // import bg from '../../img/aston-martin.jpeg';
-
-import { Canvas } from '@react-three/fiber';
+import ModelEngine from '../ModelEngine/ModelEngine';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import Engine from '../Engine/Engine';
 import { OrbitControls, Stage } from '@react-three/drei';
+import { useRef } from 'react';
+import gif from '/v8.gif';
 
 const AboutPage = () => {
+  // const { camera } = useThree();
+  // const mesh = useRef<any>();
+
+  // useFrame(({ clock }) => {
+  //   camera.position.x = Math.sin(clock.getElapsedTime()) * 3; // Изменение позиции камеры по оси X
+  //   camera.position.y = Math.cos(clock.getElapsedTime()) * 3; // Изменение позиции камеры по оси Y
+  //   camera.lookAt(mesh.current.position);
+  // });
+
   return (
     <section
       // ref={ref}
       style={{
-        // backgroundImage: `url(${bg}) `,
-        // backgroundSize: 'cover',
-        // backgroundPosition: 'center',
-        backgroundColor: 'red',
+        backgroundColor: 'rgb(4, 3, 4)',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
         height: '100vh',
-        // marginBottom: '200px',
         overflowY: 'hidden',
       }}
     >
       <Box
         component="div"
+        width="100%"
         sx={{
-          borderRadius: '2px solid white',
-          height: '100%',
           display: 'flex',
-          alignItems: 'center',
           justifyContent: 'center',
+          // alignItems: 'center',
         }}
       >
-        <Box component="div">
-          <Canvas>
-            <Stage environment="city" intensity={0.1} scale={10}>
-              <Engine />
-            </Stage>
-            <OrbitControls
-              enableZoom={false}
-              enablePan={true}
-              enableRotate={true}
-              zoomSpeed={0.6}
-              panSpeed={0.6}
-              rotateSpeed={0.6}
-              target={[0, 0, 0]} // Цель, вокруг которой вращается камера
-            />
-          </Canvas>
+        <Box
+          component="div"
+          sx={{
+            borderRadius: '2px solid white',
+            height: '100%',
+          }}
+        >
+          <img className="earth" src={gif} alt="Earth" width={'350px'} />
         </Box>
       </Box>
 
