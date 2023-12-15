84e041bf9b493b49d34b1a99ae19734b45b1dc10
ErmakovEv <ej-88@ya.ru>
2023-12-15 15:32:07 +0300

    img center

--- src/components/Pages/AboutPage.tsx
+++ src/components/Pages/AboutPage.tsx
@@ -1,53 +1,42 @@
-import { Box } from '@mui/material';
-// import CustomForm from '../CustomAutocomplite/CustomAutocomplite';
-import Footer from '../Footer/Footer';
-// import bg from '../../img/aston-martin.jpeg';
-import ModelEngine from '../ModelEngine/ModelEngine';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import Engine from '../Engine/Engine';
-import { OrbitControls, Stage } from '@react-three/drei';
-import { useRef } from 'react';
-import gif from '/v8.gif';
+import { Box } from "@mui/material";
+import Footer from "../Footer/Footer";
+import gif from "/v8.gif";
 
 const AboutPage = () => {
-  // const { camera } = useThree();
-  // const mesh = useRef<any>();
-
-  // useFrame(({ clock }) => {
-  //   camera.position.x = Math.sin(clock.getElapsedTime()) * 3; // Изменение позиции камеры по оси X
-  //   camera.position.y = Math.cos(clock.getElapsedTime()) * 3; // Изменение позиции камеры по оси Y
-  //   camera.lookAt(mesh.current.position);
-  // });
-
   return (
     <section
-      // ref={ref}
       style={{
-        backgroundColor: 'rgb(4, 3, 4)',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        height: '100vh',
-        overflowY: 'hidden',
-      }}
-    >
+        backgroundColor: "rgb(4, 3, 4)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center", // Центрирование по горизонтали
+        height: "100vh",
+        overflowY: "hidden",
+      }}>
       <Box
         component="div"
-        width="100%"
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          // alignItems: 'center',
-        }}
-      >
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh",
+          marginTop: "7em",
+        }}>
         <Box
           component="div"
           sx={{
-            borderRadius: '2px solid white',
-            height: '100%',
-          }}
-        >
-          <img className="earth" src={gif} alt="Earth" width={'350px'} />
+            borderRadius: "2px solid white",
+            width: "100%",
+          }}>
+          <img
+            style={{ margin: "0 auto", display: "block" }}
+            className="motor"
+            src={gif}
+            alt="Earth"
+            width={"350px"}
+          />
         </Box>
       </Box>
 
